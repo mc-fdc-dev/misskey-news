fedfd47255ca1e2a95b3956bebdfb68036cb54c1
mc_fdc <Masato.04.11.2007@gmail.com>
2023-07-23 14:03:00 +0000

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import Parser, { Item } from "rss-parser";
 import { config } from "dotenv";
+import Keyv from "keyv";
 
 config();
 let parser = new Parser();
 
 var lastItems: string[] = [];
+const keyv = new Keyv(process.env.DB);
+const sleep = (waitTime: number) => new Promise( resolve => setTimeout(resolve, waitTime) );
 
 async function check(): Promise<undefined> {
     let feed = await parser.parseURL('https://news.yahoo.co.jp/rss/topics/top-picks.xml');
     feed.items.forEach(async (item: Item) => {
-        if (!lastItems.includes(item.link as string)) {
+        if (!await keyv.get(item.link as string)) {
             console.log(item)
-            lastItems.push(item.link as string);
+            await keyv.set(item.link as string, item.title);
             await fetch(
                 "https://social.tuna2134.dev/api/notes/create",
                 {
@@ -32,6 +35,7 @@ async function check(): Promise<undefined> {
 async function main() {
     while (true) {
         await check();
+        await sleep(180);
     }
 };
 
